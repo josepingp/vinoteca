c05ce7e06731e83a1f76c642cd8913a78aa7e85a
josepingp <josepingp@gmail.com>
2024-08-27 19:44:10 +0200

    feat: :art: fn index at category controller

--- app/Http/Controllers/Wine/CategoryController.php
+++ app/Http/Controllers/Wine/CategoryController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Wine;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Category\CategoryRepositoryInteface;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
+    public function __construct(
+        private readonly CategoryRepositoryInteface $repository
+    ) {
+    }
 
+    public function index(): View
+    {
+        return view('wine.category.index', [
+            'categories' => $this->repository->paginate(
+                counts: ['wines'],
+            )
+        ]);
+    }
 }
