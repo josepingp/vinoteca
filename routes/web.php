937c6f1067e50dd12d787bdc1a3a57c03123a0f3
josepingp <josepingp@gmail.com>
2024-08-27 15:53:59 +0200

    feat: :art: Define routes and create Controllers.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\Wine\CategoryController;
+use App\Http\Controllers\WineController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +21,29 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+Route::group(['middleware' => 'auth', 'verified'], function () {
+    Route::resource('categories', CategoryController::class)->except(['show']);
+
+    Route::resource('wines', WineController::class)->except(['show']);
+
+    Route::prefix('shop')->name('shop.')->group(function () {
+        Route::get('/', [ShopController::class, 'index'])->name('index');
+        Route::post('/add-to-cart', [ShopController::class, 'addToCart'])->name('addToCart');
+        Route::post('/increment', [ShopController::class, 'increment'])->name('increment');
+        Route::post('/decrement', [ShopController::class, 'decrement'])->name('decrement');
+        Route::post('/remove', [ShopController::class, 'remove'])->name('remove');
+    });
+
+    Route::prefix('cart')->name('cart.')->group(function () {
+        Route::get('/', [CartController::class, 'index'])->name('index');
+        Route::post('/increment', [CartController::class, 'increment'])->name('increment');
+        Route::post('/decrement', [CartController::class, 'decrement'])->name('decrement');
+        Route::post('/remove', [CartController::class, 'remove'])->name('remove');
+        Route::post('/clear', [CartController::class, 'clear'])->name('clear');
+    });
+
+
+});
