330545e701cf77fc09c75b9f6ccd1bfaf1926eea
josepingp <josepingp@gmail.com>
2024-08-29 18:48:52 +0200

    feat: :art: Wine Request

---
+++ app/Http/Requests/WineRequest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class WineRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $imageRules = 'sometimes|image|mimes:png,jpg,jpeg|max:2048';
+
+        if ($this->isMethod('POST')) {
+            $imageRules = 'required|image|mimes:png,jpg,jpeg|max:2048';
+        }
+
+        return [
+            'category_id' => 'required|exists:categories,id',
+            'name' => ['required', 'string', 'max:255', Rule::unique('wines', 'name')->ignore($this->route('wine'))],
+            'description' => 'required|string|max:2000',
+            'year' => ['required', 'integer', 'min:' . now()->subYears(100), 'max:' . now()->year],
+            'price' => 'required|numeric|min:0',
+            'stock' => 'required|integer|min:0',
+            'image' => $imageRules,
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'El nombre es requerido.',
+            'name.string' => 'El nombre debe ser una cadena de texto.',
+            'name.max' => 'El nombre no puede ser mayor de 255 caracteres.',
+            'description.required' => 'La descripción es requerida.',
+            'description.string' => 'La descripción debe ser una cadena de texto.',
+            'description.max' => 'La descripción no puede ser mayor de 2000 caracteres.',
+            'year.required' => 'El año es requerido.',
+            'year.integer' => 'El año debe ser un número entero.',
+            'price.required' => 'El precio es requerido.',
+            'price.numeric' => 'El precio debe ser un número.',
+            'stock.required' => 'El stock es requerido.',
+            'stock.integer' => 'El stock debe ser un número entero.',
+            'image.image' => 'La imagen debe ser una imagen.',
+            'image.mimes' => 'La imagen debe ser una imagen de tipo PNG, JPG o JPEG.',
+            'image.max' => 'La imagen no puede ser mayor de 2 MB.',
+            'image.required' => 'La imagen es requerida.',
+            'category_id.exists' => 'La categoría no existe.',
+            'category_id.required' => 'La categoría es requerida.',
+            'stock.min' => 'El stock debe ser mayor o igual a 0.',
+            'price.min' => 'El precio debe ser mayor o igual a 0.',
+            'year.min' => 'El año debe ser mayor o igual a ' . now()->subYears(100),
+            'year.max' => 'El año debe ser menor o igual a ' . now()->year,
+        ];
+    }
+}
--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Repositories\Category\CategoryRepositoryInteface;
 use App\Repositories\Category\EloquentCategoryRepository;
+use App\Repositories\Wine\EloquentWineRepository;
+use App\Repositories\Wine\WineRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -16,6 +18,10 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(
             CategoryRepositoryInteface::class,
             EloquentCategoryRepository::class
+        );
+        $this->app->bind(
+            WineRepositoryInterface::class,
+            EloquentWineRepository::class
         );
     }
 
