a3de0c9aa965f7682fd454a30a87cbaaf905c09e
josepingp <josepingp@gmail.com>
2024-08-29 22:32:43 +0200

    feat: :art: wine index fn

--- app/Http/Controllers/WineController.php
+++ app/Http/Controllers/WineController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\Wine\WineRepositoryInterface;
 use Illuminate\Http\Request;
 
 class WineController extends Controller
 {
+    public function __construct(private readonly WineRepositoryInterface $repository)
+    {
+    }
+
+    public function index()
+    {
+        return view('wines.index', [
+            'wines' => $this->repository->paginate(
+                relationships: ['category'],
+            )
+        ]);
+    }
+
+
+
 
 }
