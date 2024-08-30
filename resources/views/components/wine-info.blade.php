a6e97122b6474f768786fe464a27205806a31e17
josepingp <josepingp@gmail.com>
2024-08-30 16:44:34 +0200

    feat: :art: create diferent wine components

---
+++ resources/views/components/wine-info.blade.php
@@ -0,0 +1,17 @@
+@props(['wine'])
+
+<p class="mb-2 font-normal text-gray-700 dark:text-gray-400">
+    {{ __('Precio unidad') }}: {{ $wine->formatted_price }}
+</p>
+
+<p class="mb-2 font-normal text-gray-700 dark:text-gray-400">
+    {{ __('Cosecha') }}: {{ $wine->year }}
+</p>
+
+<p class="mb-2 font-normal text-gray-700 dark:text-gray-400">
+    {{ __('Stock') }}: {{ $wine->stock }}
+</p>
+
+<p class="mb-2 font-normal text-gray-700 dark:text-gray-400">
+    {{ $wine->description }}
+</p>
---
+++ resources/views/components/wine-name-and-category.blade.php
@@ -0,0 +1,8 @@
+@props(['wine']);
+
+<h5 class="text-2xl font-bold tracking-tight text-gray-900 mb-2 dark:text-white">
+    {{ $wine->name }}
+    <span class="text-sm text-gray-500 dark:text-gray-400 font-normal">
+        {{ __('Categoría') }}: {{ $wine->category->name }}
+    </span>
+</h5>
