1221eb28daa61b1fb711262a7d80cba6f08b9934
josepingp <josepingp@gmail.com>
2024-09-02 15:12:56 +0200

    feat: :sparkles: start to develop shop controller

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\Shop\ShopRepositoryInterface;
+use App\Services\Cart;
+use App\Traits\CartActions;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ShopController extends Controller
 {
-    //
+    use CartActions;
+
+    public function __construct(
+        private readonly ShopRepositoryInterface $repository,
+        private readonly Cart $cart
+    ) {
+    }
+
+    public function index(): View
+    {
+        $wines = $this->repository->paginate();
+
+        return view('shop.index', compact('wines'));
+    }
+
+
+
+
 }
